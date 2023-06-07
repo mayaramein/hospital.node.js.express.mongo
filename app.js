85e891b585b7cd8d7cc93ba76b16e6ecfd0db375
Mayar Amein <mayaramein@outlook.com>
2023-06-07 11:01:25 +0300

    commit

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const app = express();
-const db = require('./config/db'); 
+const connectDB = require('./config/db'); 
 
 // Set up middleware
 app.use(express.json());
@@ -34,9 +34,97 @@ app.get('/', (req, res) => {
   res.send('Hello, world!');
 });
 
-
+// Call connectDB function to establish the database connection
+connectDB();
 
   const port = process.env.PORT || 3000;
 app.listen(port, () => {
   console.log(`Server listening on port ${port}`);
 });
+
+// [ {
+//   doctor: ObjectId("6480301ab2f657830a106f9e"),
+//   patient: ObjectId("648030a5b2f657830a106fa5"),
+//   appointmentDateTime: ISODate("2023-06-01T10:00:00Z"),
+//   status: "confirmed"
+// },
+// {
+//   doctor: ObjectId("6480301ab2f657830a106f9f"),
+//   patient: ObjectId("648030a5b2f657830a106fa4"),
+//   appointmentDateTime: ISODate("2023-06-02T14:30:00Z"),
+//   status: "pending"
+// },
+// {
+//   doctor: ObjectId("6480301ab2f657830a106fa0"),
+//   patient: ObjectId("648030a5b2f657830a106fa6"),
+//   appointmentDateTime: ISODate("2023-06-03T11:45:00Z"),
+//   status: "confirmed"
+// },]
+
+// [
+//   {
+//     "title": "New Hospital Wing Opening",
+//     "description": "We are excited to announce the opening of our new hospital wing, equipped with state-of-the-art facilities.",
+//     "publicationDate": ISODate("2023-05-30T00:00:00Z"),
+//     "author": "Hospital Administration",
+//     "relatedImages": ["image1.jpg", "image2.jpg"]
+//   },
+//   {
+//     "title": "Health Tips for Summer",
+//     "description": "Stay hydrated and protect yourself from the sun. Follow these health tips to have a safe and enjoyable summer.",
+//     "publicationDate": ISODate("2023-06-01T00:00:00Z"),
+//     "author": "Dr. Jane Smith",
+//     "relatedImages": ["image3.jpg"]
+//   },
+//   {
+//     "title": "Community Health Seminar",
+//     "description": "Join us for a community health seminar on June 15th to learn about common health issues and prevention strategies.",
+//     "publicationDate": ISODate("2023-06-03T00:00:00Z"),
+//     "author": "Hospital Outreach Team",
+//     "relatedImages": ["image4.jpg"]
+//   }
+// ]
+
+
+// [
+//   {
+//     name: "John Doe",
+//     email: "johndoe@example.com",
+//     message: "I have a question about your services.",
+//     submittedAt: ISODate("2023-06-01T08:30:00Z")
+//   },
+//   {
+//     name: "Jane Smith",
+//     email: "janesmith@example.com",
+//     message: "I would like to inquire about appointment availability.",
+//     submittedAt: ISODate("2023-06-02T15:20:00Z")
+//   },
+//   {
+//     name: "David Johnson",
+//     email: "davidjohnson@example.com",
+//     message: "I need assistance with my medical records.",
+//     submittedAt: ISODate("2023-06-03T10:15:00Z")
+//   },
+// ]
+
+// [
+//   {
+//     name: "Cardiology",
+//     description: "Specialized care for heart-related conditions",
+//     department: "Medical",
+//     doctors: [ObjectId("6480301ab2f657830a106f9e"), ObjectId("6480301ab2f657830a106f9f")]
+//   },
+//   {
+//     name: "Pediatrics",
+//     description: "Medical care for infants, children, and adolescents",
+//     department: "Medical",
+//     doctors: [ObjectId("6480301ab2f657830a106f9e")]
+//   },
+//   {
+//     name: "Orthopedics",
+//     description: "Treatment for musculoskeletal conditions and injuries",
+//     department: "Surgical",
+//     doctors: [ObjectId("6480301ab2f657830a106fa0")]
+//   }
+// ]
+
--- routes/index.js
+++ routes/index.js
@@ -1,2 +1,11 @@
 const express = require('express');
 const router = express.Router();
+
+// Index route
+router.get('/', (req, res) => {
+  // Handle the index route logic here
+  res.send('Welcome to the index page');
+});
+
+// Export the router
+module.exports = router;
